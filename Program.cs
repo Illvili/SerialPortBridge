770ed57925c94af8bb81cd1e739faf84a0ca4e68
Illvili <rainyloved@gmail.com>
2013-02-18 14:53:49 +0800

    Split read and write thread
    经测试由于延迟还是不能完成操作

--- Program.cs
+++ Program.cs
@@ -10,76 +10,121 @@ namespace SerialPortBridge
 {
     class Program
     {
+        static NamedPipeServerStream npipe;
+        static SerialPort comport;
+
+        static Queue<byte> data = new Queue<byte>(1024);
+
+        static DateTime last_read_time;
+
         static void Main(string[] args)
         {
-            SerialPort comport = new SerialPort("COM1");
+            comport = new SerialPort("COM1");
 
-            byte[] buffer = new byte[1024];
-            int buffer_len = 0;
-            int buffer_read = 0;
-            int buffer_read_times = 0;
+            Thread t_NamedPipeReadThread = new Thread(NamedPipeReadThread);
+            Thread t_SerialPortWriteThread = new Thread(SerialPortWriteThread);
+
+            t_NamedPipeReadThread.Start();
+            t_SerialPortWriteThread.Start();
+        }
+
+        static void NamedPipeReadThread()
+        {
+            byte[] np_buffer = new byte[1024];
+            int np_buffer_len = 0;
 
             while (true)
             {
-                NamedPipeServerStream npipe = new NamedPipeServerStream("spb", PipeDirection.InOut);
-                Console.Write("Waiting for connection...");
+                npipe = new NamedPipeServerStream("spb", PipeDirection.InOut, 1, PipeTransmissionMode.Byte, PipeOptions.Asynchronous);
+                Console.WriteLine("Waiting for connection...");
                 npipe.WaitForConnection();
-                Console.WriteLine("Ok");
+                Console.WriteLine("NP connected!");
 
                 while (true)
                 {
                     while (npipe.CanRead)
                     {
-                        buffer_len = 0;
-                        buffer_read = 0;
-                        buffer_read_times = 0;
-                        Console.WriteLine("Waiting for data...");
+                        IAsyncResult result = npipe.BeginRead(np_buffer, 0, np_buffer.Length, null, null);
+                        np_buffer_len = npipe.EndRead(result);
+                        if (0 == np_buffer_len) break;
 
-                        //do
-                        //{
-                        //    buffer_read = npipe.Read(buffer, buffer_len, buffer.Length - buffer_len);
-                        //    buffer_len += buffer_read;
-                        //} while (0 != buffer_read);
-                        //if (0 == buffer_len) break;
-
-                        buffer_len = npipe.Read(buffer, 0, buffer.Length);
-                        if (0 == buffer_len) break;
-
-                        Console.Write("<<< ");
-                        for (int i = 0; i < buffer_len; i++)
+                        lock (data)
                         {
-                            Console.Write(((byte)buffer[i]).ToString("X2"));
-                        }
+                            last_read_time = DateTime.Now;
 
-                        comport.Open();
-                        comport.Write(buffer, 0, buffer_len);
-                        Console.WriteLine();
-
-                        int c;
-                        buffer_len = 0;
-                        comport.ReadTimeout = 10;
-                        Console.Write(">>> ");
-                        try
-                        {
-                            while (true)
+                            for (int i = 0; i < np_buffer_len; i++)
                             {
-                                c = comport.ReadByte();
-                                buffer[buffer_len++] = (byte)c;
-                                Console.Write(c.ToString("X2"));
+                                data.Enqueue((byte)np_buffer[i]);
+                                Console.WriteLine("<<< {0:X2}", (byte)np_buffer[i]);
                             }
                         }
-                        catch (TimeoutException) { }
-
-                        npipe.Write(buffer, 0, buffer_len);
-                        Console.WriteLine("\n");
-                        comport.Close();
-
-                        npipe.WaitForPipeDrain();
                     }
+
                     if (!npipe.IsConnected) break;
                 }
+
                 Console.WriteLine("Connection aborted!\n\n");
                 npipe.Close();
+            }
+        }
+
+        static void SerialPortWriteThread()
+        {
+            byte[] sp_buffer = new byte[1024];
+            int sp_buffer_len = 0;
+
+            while (true)
+            {
+                while (0 == data.Count)
+                {
+                    if (0 == data.Count)
+                    {
+                        Thread.Sleep(500);
+                    }
+                    else if (((DateTime.Now - last_read_time).TotalMilliseconds < 50))
+                    {
+                        Thread.Sleep(50);
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+
+                lock (comport)
+                {
+                    Console.WriteLine("Write to serial port...");
+                    comport.Open();
+                    comport.Write(data.ToArray(), 0, data.Count);
+                    Console.WriteLine("Write {0} bytes!", data.Count);
+                    data.Clear();
+                }
+                Console.WriteLine("Read from serial port...");
+                int c;
+                sp_buffer_len = 0;
+                comport.ReadTimeout = 500;
+                try
+                {
+                    while (true)
+                    {
+                        c = comport.ReadByte();
+                        sp_buffer[sp_buffer_len++] = (byte)c;
+                        Console.WriteLine(">>> {0:X2}", (byte)c);
+                    }
+                }
+                catch (TimeoutException) { }
+                comport.Close();
+
+                // Console.Write(">>> ");
+                // for (int i = 0; i < sp_buffer_len; i++)
+                // {
+                //     Console.Write("{0:X2} ", (byte)sp_buffer[i]);
+                // }
+                // Console.WriteLine();
+                // 
+                IAsyncResult result = npipe.BeginWrite(sp_buffer, 0, sp_buffer_len, null, null);
+                npipe.EndWrite(result);
+                npipe.WaitForPipeDrain();
             }
         }
     }
